a5eaf6454168e1621bd1eaa06a5df29a644a76f6
DarkseeTRD <darksee04@gmail.com>
2023-08-31 09:51:31 +1000

    Started next button

---
+++ Assets/NextButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextButton : MonoBehaviour
+{
+    public Sprite HoverSprite;
+    public Image ButtonImage;
+    public void onPointerEnter 
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    
+   
+        
+    
+}
