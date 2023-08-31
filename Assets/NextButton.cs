842eec5dc9c38ab41bd34590da4480f8714d1439
DarkseeTRD <darksee04@gmail.com>
2023-08-31 11:19:30 +1000

    uibutton

--- Assets/NextButton.cs
+++ Assets/NextButton.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class NextButton : MonoBehaviour
+public class NextButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Sprite HoverSprite;
-    public Image ButtonImage;
-    public void onPointerEnter 
+    public Image ButtonImage; 
+    public Sprite StationarySprite;
+
 
 
 
@@ -17,12 +19,23 @@ public class NextButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ButtonImage = GetComponent<Image>();
+        StationarySprite = ButtonImage.sprite; 
         
     }
 
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        ButtonImage.sprite = HoverSprite;
+
+    }    
+   public void OnPointerExit (PointerEventData eventData) 
+    
+   {
+        ButtonImage.sprite = StationarySprite;
     
     
-   
+    }
         
     
 }
